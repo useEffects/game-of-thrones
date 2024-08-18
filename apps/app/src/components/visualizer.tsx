101dc384f4756ec7706dff274311319d711dc809
useEffects <joel.sr1024@gmail.com>
2024-08-18 12:19:31 +0530

    build success

--- apps/app/src/components/visualizer.tsx
+++ apps/app/src/components/visualizer.tsx
@@ -1,11 +1,8 @@
 import { Box, SxProps, Tab, Tabs, Typography } from "@mui/material";
-import dagre from 'dagre';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Highlight from "react-highlight";
-import { Node } from 'reactflow';
 import useUser from "src/hooks/user";
 import { sortMessagesReverse } from "src/lib/helpers";
-import * as yup from 'yup';
 import Flow from "./flow";
 import { IBotMessage, SenderType } from "./message-bubble";
 
@@ -58,77 +55,77 @@ function RenderJsonResult(props: { cypherResult?: string }) {
 
 }
 
-type CypherResultFromLLM = {
-    identity: number;
-    labels: string[];
-    properties: {
-        name: string;
-        id: string;
-    };
-    elementId: string;
-}
+// type CypherResultFromLLM = {
+//     identity: number;
+//     labels: string[];
+//     properties: {
+//         name: string;
+//         id: string;
+//     };
+//     elementId: string;
+// }
 
-const cypherResultFromLLMSchema = yup.object().shape({
-    identity: yup.number().required().positive().integer(),
-    labels: yup.array().of(yup.string().required()).required(),
-    properties: yup.object().shape({
-        name: yup.string().required(),
-        id: yup.string().required()
-    }).required(),
-    elementId: yup.string().required()
-});
+// const cypherResultFromLLMSchema = yup.object().shape({
+//     identity: yup.number().required().positive().integer(),
+//     labels: yup.array().of(yup.string().required()).required(),
+//     properties: yup.object().shape({
+//         name: yup.string().required(),
+//         id: yup.string().required()
+//     }).required(),
+//     elementId: yup.string().required()
+// });
 
-enum ResultType {
-    LLM = "llm",
-    Neo4j = "neo4j"
-}
+// enum ResultType {
+//     LLM = "llm",
+//     Neo4j = "neo4j"
+// }
 
-const useCypherResult = (cypherResult?: string) => {
-    const [parsedResult, setParsedResult] = useState<{
-        type: ResultType,
-        data: CypherResultFromLLM[]
-    }>();
+// const useCypherResult = (cypherResult?: string) => {
+//     const [parsedResult, setParsedResult] = useState<{
+//         type: ResultType,
+//         data: CypherResultFromLLM[]
+//     }>();
 
-    useEffect(() => {
-        async function validateCypherResult() {
-            try {
-                if (!cypherResult) return;
-                const parsedResult = JSON.parse(cypherResult);
-                if (Array.isArray(parsedResult)) {
-                    const result = await Promise.all(parsedResult.map(pr => cypherResultFromLLMSchema.validate(pr)));
-                    setParsedResult({
-                        type: ResultType.LLM,
-                        data: result
-                    });
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        validateCypherResult()
-    }, [cypherResult])
-    return [parsedResult, setParsedResult] as const;
-}
+//     useEffect(() => {
+//         async function validateCypherResult() {
+//             try {
+//                 if (!cypherResult) return;
+//                 const parsedResult = JSON.parse(cypherResult);
+//                 if (Array.isArray(parsedResult)) {
+//                     const result = await Promise.all(parsedResult.map(pr => cypherResultFromLLMSchema.validate(pr)));
+//                     setParsedResult({
+//                         type: ResultType.LLM,
+//                         data: result
+//                     });
+//                 }
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         }
+//         validateCypherResult()
+//     }, [cypherResult])
+//     return [parsedResult, setParsedResult] as const;
+// }
 
-const mapToReactFlowNodes = (data: CypherResultFromLLM[]): Node[] => {
-    const g = new dagre.graphlib.Graph();
-    g.setGraph({});
-    g.setDefaultEdgeLabel(() => ({}));
+// const mapToReactFlowNodes = (data: CypherResultFromLLM[]): Node[] => {
+//     const g = new dagre.graphlib.Graph();
+//     g.setGraph({});
+//     g.setDefaultEdgeLabel(() => ({}));
 
-    data.forEach(item => {
-        g.setNode(item.identity.toString(), { label: item.properties.name });
-    });
+//     data.forEach(item => {
+//         g.setNode(item.identity.toString(), { label: item.properties.name });
+//     });
 
-    dagre.layout(g);
+//     dagre.layout(g);
 
-    return data.map(item => {
-        const node = g.node(item.identity.toString());
-        return {
-            id: item.identity.toString(),
-            data: {
-                label: `${item.properties.name} (${item.labels.join(", ")})`,
-            },
-            position: { x: node.x, y: node.y },
-        };
-    });
-};
+//     return data.map(item => {
+//         const node = g.node(item.identity.toString());
+//         return {
+//             id: item.identity.toString(),
+//             data: {
+//                 label: `${item.properties.name} (${item.labels.join(", ")})`,
+//             },
+//             position: { x: node.x, y: node.y },
+//         };
+//     });
+// };
