688c3b698ef6280f65d388e678b1a676500fb6a2
useEffects <joel.sr1024@gmail.com>
2024-08-18 12:50:02 +0530

    update python paths

--- functions/neo4j-api/api/index.py
+++ functions/neo4j-api/api/index.py
@@ -1,7 +1,42 @@
 from http.server import BaseHTTPRequestHandler
 import json
-from lib import fetch_response
+import os
+from neo4j import GraphDatabase, Result
 
+NEO4J_URI = os.environ.get("NEO4J_URI")
+NEO4J_USERNAME = os.environ.get("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD")
+
+def fetch_response(query):
+    with GraphDatabase.driver(
+        NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)
+    ) as driver:
+        driver.verify_connectivity()
+        query_result = driver.execute_query(
+            query_=query,
+            database_="neo4j",
+            result_transformer_=Result.graph,
+        )
+        nodes = []
+        relationships = []
+        for node in query_result._nodes.items():
+            nodes.append(
+                {
+                    "id": node[1].id,
+                    "labels": list(node[1].labels),
+                    "properties": {key: value for key, value in node[1].items()},
+                }
+            )
+        for relationship in query_result._relationships.items():
+            relationships.append(
+                {
+                    "id": relationship[1].id,
+                    "type": relationship[1].type,
+                    "start_node": relationship[1].start_node.id,
+                    "end_node": relationship[1].end_node.id,
+                }
+            )
+        return {"nodes": nodes, "relationships": relationships}
 
 class handler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
