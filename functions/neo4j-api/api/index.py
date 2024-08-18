f32e6843e321e51f99c5eef15ac2f4d57f7fa313
useEffects <joel.sr1024@gmail.com>
2024-08-18 13:38:01 +0530

    update cors configuration

--- functions/neo4j-api/api/index.py
+++ functions/neo4j-api/api/index.py
@@ -65,5 +65,5 @@ class handler(BaseHTTPRequestHandler):
 
     def add_cors_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
