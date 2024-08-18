aabd30a5d508d31c0c5f60cb0c394fbf014aed0d
useEffects <joel.sr1024@gmail.com>
2024-08-18 12:36:50 +0530

    update prod api route names

--- apps/app/src/lib/constants.ts
+++ apps/app/src/lib/constants.ts
@@ -1,4 +1,4 @@
 const isDev = process.env.NODE_ENV === 'development';
 
-export const langchainServer = isDev ? "http://localhost:8000" : ""
-export const neo4jApi = isDev ? "http://localhost:8001" : ""
+export const langchainServer = isDev ? "http://localhost:8000" : "https://langchain.got.joelsamuel.me"
+export const neo4jApi = isDev ? "http://localhost:8001" : "https://neo4j-api.got.joelsamuel.me"
